33d6b21443a043d65b1e9f42610002b40c992244
pablovilchez <pablovilchez.r@gmail.com>
2024-12-12 21:23:48 +0100

    refactor

--- lib/domain/repositories/contact_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../entities/contact.dart';
-
-abstract class ContactRepository {
-  Future<List<Contact>> getContacts();
-  Future<void> addContact(Contact contact);
-  Future<void> updateContact(Contact contact);
-  Future<void> deleteContact(Contact contact);
-}
